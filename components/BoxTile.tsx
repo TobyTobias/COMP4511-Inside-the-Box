69c3fe57c70b91028ca0fc862ff85f0ec7f9e48e
Toby <tobias.reid@blackdog.org.au>
2020-12-05 09:18:03 +1100

    Box Tile UI improvements and more box data

--- components/BoxTile.tsx
+++ components/BoxTile.tsx
@@ -7,59 +7,178 @@ import {
   TouchableHighlight
 } from "react-native";
 import { BodyText } from './ui/StyledText';
+import { Icon } from 'react-native-elements';
 
 export default function BoxTile({
   box,
   onPress = f => f
 }) {
+  console.log('box: ' + JSON.stringify(box));
+  // have to map db images?? https://stackoverflow.com/questions/30854232/react-native-image-require-module-using-dynamic-names
+  var dynamicPath = getImagePath(box.image_name);
+
   return (
     <TouchableHighlight
       style={styles.button}
       onPress={() => {onPress(box)}}
-      underlayColor="#E47E6D"
+      underlayColor="rgba(255,255,255,0.9)"
     >
-      <View style={styles.row}>
-        <Image
-          style={styles.profilePicture}
-          source={require('../assets/images/icon.png')}
-        />
-        <View style={styles.textContainer}>
-          <BodyText style={styles.buttonText}>
-            {box.name}
-          </BodyText>
+      <>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.boxPicture}
+            source={dynamicPath}
+          />
         </View>
-      </View>
+        <View style={styles.textContainer}>
+          <View style={styles.descriptionContainer}>
+            <Text style={styles.description}>
+              {box.name}
+            </Text>
+          </View>
+          <View style={styles.textFooter}>
+            <Text style={styles.boxPrice}>
+              ${box.price}
+            </Text>
+            <Icon
+              color="#000"
+              type="ionicon"
+              name="ios-heart"
+              size={30}
+              onPress={() =>{}}
+            />
+          </View>
+        </View>
+      </>
     </TouchableHighlight>
   );
 }
 
+function getImagePath(image_name) {
+  console.log("getImagePath: " + image_name);
+  var image_path;
+  switch (image_name) {
+    case "diysuccgarden.jpg":
+      image_path = require('../assets/images/boxes/diysuccgarden.jpg');
+      break;
+    case "cocktailmixer.jpg":
+      image_path = require('../assets/images/boxes/cocktailmixer.jpg');
+      break;
+    case "paella.jpg":
+      image_path = require('../assets/images/boxes/paella.jpg');
+      break;
+    case "sushi1.jpg":
+      image_path = require('../assets/images/boxes/sushi1.jpg');
+      break;
+    case "whittling.png":
+      image_path = require('../assets/images/boxes/whittling.png');
+      break;
+    case "KnitBeginners.jpg":
+      image_path = require('../assets/images/boxes/KnitBeginners.jpg');
+      break;
+    case "yoga2.jpg":
+      image_path = require('../assets/images/boxes/yoga2.jpg');
+      break;
+    case "soccer2.jpg":
+      image_path = require('../assets/images/boxes/soccer2.jpg');
+      break;
+    case "rockpainting.jpg":
+      image_path = require('../assets/images/boxes/rockpainting.jpg');
+      break;
+    case "Badminton.jpg":
+      image_path = require('../assets/images/boxes/Badminton.jpg');
+      break;
+    case "KimchiKit.jpg":
+      image_path = require('../assets/images/boxes/KimchiKit.jpg');
+      break;
+    case "SucculentTerrarium.jpg":
+      image_path = require('../assets/images/boxes/SucculentTerrarium.jpg');
+      break;
+    case "KnitVeggie.jpg":
+      image_path = require('../assets/images/boxes/KnitVeggie.jpg');
+      break;
+    case "SucculentBirdhouse.jpg":
+      image_path = require('../assets/images/boxes/SucculentBirdhouse.jpg');
+      break;
+    case "KnitFrogBiology.jpg":
+      image_path = require('../assets/images/boxes/KnitFrogBiology.jpg');
+      break;
+    case "SucculentAnimals.jpg":
+      image_path = require('../assets/images/boxes/SucculentAnimals.jpg');
+      break;
+    case "KnitLarge.jpg":
+      image_path = require('../assets/images/boxes/KnitLarge.jpg');
+      break;
+    default:
+      image_path = require('../assets/images/icon.png');
+  }
+  console.log("getImagePath image_path: " + image_path);
+  return image_path;
+}
+
 const styles = StyleSheet.create({
   button: {
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
+    flex: 1,
+    flexDirection: "row",
+    margin: 5,
+    marginLeft: 10,
+    marginRight: 10,
     borderRadius: 10,
-    // alignSelf: "stretch",
     backgroundColor: "rgba(255, 255, 255, 1)",
+    elevation: 4,
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    shadowOffset: {
+        height: 0,
+        width: 0
+    },
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    backgroundColor: "white",
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  boxPicture: {
+    width: 100,
+    height: 100,
+    backgroundColor: "white",
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
   },
   textContainer: {
-    margin: 20,
-    flex: 1,
-    alignItems: 'flex-start'
+    position: 'absolute',
+    left: 100,
+    right: 0,
+    height: '100%',
+    // flex: 1,
+    padding: 5,
+    flexDirection: "column",
+    alignItems: 'flex-start',
   },
-  buttonText: {
-    fontSize: 30,
-    textAlign: "center"
+  descriptionContainer: {
+    flexDirection: "column",
+    alignItems: 'flex-start',
+    flex: 1,
+    fontSize: 14
+  },
+  description: {
+    flex: 1, 
+    flexWrap: 'wrap'
+  },
+  textFooter: {
+    flexDirection: "row", 
+    justifyContent: "space-between",
+    alignItems: 'flex-end'
+  },
+  boxPrice: {
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    flex: 1,
+    fontWeight: 'bold'
   },
   row: {
     flexDirection: "row",
-    alignItems: "center",
   },
-  profilePicture: {
-    height: 80,
-    width: 80,
-    margin: 5,
-    borderRadius: 40,
-    backgroundColor: "white"
-  }
 });
--- components/database.js
+++ components/database.js
@@ -44,7 +44,7 @@ const setupDatabaseAsync = async () => {
     db.transaction(
       (tx) => {
         tx.executeSql(
-          "create table if not exists boxes (id integer primary key not null, name text, description text, image_path text, price text);"
+          "create table if not exists boxes (id integer primary key not null, name text, description text, image_name text, price text);"
         );
       },
       (_, error) => {
@@ -63,11 +63,24 @@ const setupBoxesAsync = async () => {
   return new Promise((resolve, _reject) => {
     db.transaction(
       (tx) => {
-        tx.executeSql('insert into boxes (id, name, description, image_path, price) values (1, \'Succulent for beginners\', \'Succulent bulk starter kit of 6 cuttings, perfect for succulent beginners or collectors. All cuttings are made fresh from healthy, well established outdoor plants.\', \'/assets/images/succulent1.png\', 32),' + 
-          '(2, \'Knitting for beginners\', \'Stitching a scarf for your cosy winter wardrobe? Get all wrapped up with this super snuggly beginner knitting kit.\', \'/assets/images/knitting1.png\', 47),' +
-          '(3, \'Advaned Yoga\', \'This one simple kit can jump-start any yoga practice\', \'/assets/images/yoga1.png\', 14),' +
-          '(4, \'Soccer Tricks\', \'This kit lets you get fancy with soccer tricks\', \'/assets/images/soccer1.png\', 35),' +
-          '(5, \'Rock Painting\', \'Get starts with artist rock painting\', \'/assets/images/rockpainting1.png\', 18)');
+        tx.executeSql('insert into boxes (id, name, description, image_name, price) values ' +
+          '(1, \'Miniature succulent garden set\', \'Mini succulent starter kit of 6 cuttings, perfect for succulent beginners or collectors. All cuttings are made fresh from healthy, well established outdoor plants.\', \'diysuccgarden.jpg\', 31),' + 
+          '(2, \'Create the cosmopolitans of your dreams at home\', \'Includes a mixer,  dried orange slices,  shaker and measuring cup. Just add your favourite alcohol for a big night in.\', \'cocktailmixer.jpg\', 80),' + 
+          '(3, \'Gift your favourite foodie this impressive Spanish Paella kit,  packed with all the essentials\', \'Entertain family and friends with ease as you learn to cook sumptuous Spanish paella using this complete kit,  packed to perfection!\', \'paella.jpg\', 91),' + 
+          '(4, \'Create sushi magic at home in your own kitchen\', \'The kit is packed with all the equipment and ingredients you need for a delectable sushi feast including a recipe booklet and cheat sheet,  chopsticks,  rolling mats,  nori sheets,  sauces,  wasabi and ginger. Just add fresh fillings from salmon and tuna to avocado and cucumber.\', \'sushi1.jpg\', 50),' + 
+          '(5, \'Chip off the old block with this whittling kit\', \'Whittling kit with essential wood carving tools,  gloves,  case and leather apron\', \'whittling.png\', 38),' + 
+          '(6, \'Beginners sewing and knitting kit with all the essentials to get you going.\', \'Want the perfect scarf for your cosy winter wardrobe? Get all wrapped up with this super snuggly beginner knitting kit.\', \'KnitBeginners.jpg\', 25),' + 
+          '(7, \'Advanced yoga complete kit\', \'This one simple kit can jump-start any yoga practice. Includes mat, block, essential reading and DVD.\', \'yoga2.jpg\', 54),' + 
+          '(8, \'Soccer tricks to impress your friends!\', \'This kit lets you get fancy with soccer tricks. Every soccer star starts somewhere!\', \'soccer2.jpg\', 35),' + 
+          '(9, \'Rock painting kit to create a unique memento!\', \'Combine nature and artistry with this super cute rock painting kit for a group of four. Includes paintbrushes, paint set, lacquer and stencils.\', \'rockpainting.jpg\', 28),' + 
+          '(10, \'Beach badminton shuttlecock net racquets and visors kit\', \'Beach badminton shuttlecock net racquets and visors kit. What are you waiting for? Grab your friends and get out on the sand. Includes net,  four racquets,  3 shuttlecocks and 4 cool hats.\', \'Badminton.jpg\', 66),' + 
+          '(11, \'Kimchi kit! Make your own delicious spicy fermented food\', \'Kimchi kit! Make your own delicious spicy fermented food with this complete kimchi preparation kit. Arrives with fresh ingredients in a coolbag.\', \'KimchiKit.jpg\', 25),' + 
+          '(12, \'Grow your own living Mexico style desert terrarium\', \'Grow your own living Mexico style desert terrarium with these rare varieties. All cuttings are made fresh from healthy,  well established outdoor plants.\', \'SucculentTerrarium.jpg\', 28),' + 
+          '(13, \'Veggie knit kit ‚Äì create a posse of mini beetroot,  carrot,  corn and eggplant companions.\', \'Veggie knit kit ‚Äì create a posse of soft and snuggly mini companions. Yarn,  needles,  googly eyes and complete instructions for 5 veggie friends.\', \'KnitVeggie.jpg\', 14),' + 
+          '(14, \'Construct a cosy wooden birdhouse succulent planter\', \'Living succulent birdhouse planter kit. Construct a cosy home to welcome winged friends. Complete plant care instructions included. 20cm x 27cm x 22cm.\', \'SucculentBirdhouse.jpg\', 41),' + 
+          '(15, \'Biology frog knitting kit.\', \'Biology frog knitting kit. True to anatomy! Display on a cork board for an authentic look. Includes diagrams.\', \'KnitFrogBiology.jpg\', 19),' + 
+          '(16, \'Decorate resin succulent pots as cute animals\', \'Decorate resin succulent pots as cute animals. Perfect kit to enjoy with a friend or younger family member.\', \'SucculentAnimals.jpg\', 39),' + 
+          '(17, \'Up for a challenge? Knit yourself the biggest blanket ever in super thick yarn.\', \'Up for a challenge? Knit yourself the biggest blanket ever in super thick yarn. This is not for the faint hearted!\', \'KnitLarge.jpg\', 45)');
       },
       (t, error) => {
         console.log("db error insertBoxes");
