2623a5b5c960da2887f0088a64cdb3dfd4fab24b
Toby <tobias.reid@blackdog.org.au>
2020-12-06 10:02:14 +1100

    Box Search screen search input rounded border

--- screens/BoxSearchScreen.js
+++ screens/BoxSearchScreen.js
@@ -78,6 +78,7 @@ export default function BoxSearchScreen() {
             placeholder="Search"
             onChangeText={onChangeSearch}
             value={searchQuery}
+            style={{borderRadius: 10}}
           />
         </View>
       <View style={styles.boxesContainer}>
